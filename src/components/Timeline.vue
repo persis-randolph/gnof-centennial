adfa761ccfc8dcf5d433c8f8cdca4bddf9f82f6c
Persis Randolph <prandolph@demeterlogistics.com>
2022-12-24 14:49:32 -0600

    added start and end years

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="container">
     <div id="timeline-container">
+      <div id="years">
+        <div>{{ firstYear }}</div>
+        <div>{{ lastYear }}</div>
+      </div>
       <div id="timeline">{{ currentCard }}</div>
     </div>
-    <!-- :style="{'height': (windowHeight * .7) + 'px'}" -->
     <div id="card-section">
-    <!-- :style="{width: (windowWidth * 0.7) + 'px', 'height': (windowHeight * 0.715) + 'px'}" -->
       <Card
         v-for="card in cardData"
         :key="card.year"
@@ -19,7 +21,6 @@
 <script>
 import Card from './Card.vue'
 import { ref } from 'vue'
-// import { ref, onMounted, nextTick, onBeforeUnmount, watch } from 'vue'
 import data from '../data/cardData'
 
 export default {
@@ -35,44 +36,14 @@ export default {
       currentCard.value = cardYear
     }
 
-    // const windowHeight = ref(window.innerHeight + window.scrollY)
-    // const windowHeight = ref(window.innerHeight)
-    // const windowWidth = ref(window.innerWidth)
-    // const scrollYPosition = ref(window.scrollY)
-
-    // watch([windowHeight, windowWidth, scrollYPosition], () => {
-    //   console.log('height: ' + windowHeight.value + ' width: ' + windowWidth.value)
-    //   console.log('scrollY: ', scrollYPosition.value)
-    // })
-
-    // const onResize = () => {
-    //   windowHeight.value = window.innerHeight
-    //   windowWidth.value = window.innerWidth
-    // }
-
-    // const onScroll = () => {
-    //   scrollYPosition.value = window.scrollY
-    //   // windowHeight.value = window.innerHeight
-    //   // windowWidth.value = window.innerWidth
-    // }
-
-    // onMounted(() => {
-    //   nextTick(() => {
-    //     window.addEventListener('resize', onResize)
-    //     window.addEventListener('scroll', onScroll)
-    //   })
-    // })
-
-    // onBeforeUnmount(() => {
-    //   window.removeEventListener('resize', onResize)
-    //   window.removeEventListener('scroll', onScroll)
-    // })
+    const firstYear = data[0].year
+    const lastYear = data[data.length - 1].year
 
     return {
       cardData,
       currentCard,
-      // windowHeight,
-      // windowWidth,
+      firstYear,
+      lastYear,
       setCurrentCard
     }
   }
@@ -85,23 +56,20 @@ export default {
   background-color: #e2f6ff;
   padding-bottom: 20px;
 }
+
 #card-section {
-  /* overflow: scroll; */
-  /* -ms-overflow-style: none; */
-  /* scrollbar-width: none; */
   width: 80%;
   margin: 0 auto;
 }
-/* #card-section::-webkit-scrollbar { */
-  /* display: none; */
-/* } */
+
 #timeline-container {
   background-color: #04307e;
+  color: white;
   position: sticky;
   top: 0;
   width: 100%;
   z-index: 2;
-  padding: 30px 0;
+  padding: 20px 0 35px 0;
 }
 
 #timeline {
@@ -113,5 +81,12 @@ export default {
   -moz-box-shadow: 2px 2px 3px #b0a9a0;
   -webkit-box-shadow: 2px 2px 3px #b0a9a0;
   margin: auto;
+}
+
+#years {
+  display: flex;
+  width: 80%;
+  justify-content: space-between;
+  padding: 0 10%;
 }
 </style>
