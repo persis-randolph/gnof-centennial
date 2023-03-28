cd5e46cc17061e99bd0ae19a44be04327e313c9b
persis-randolph <persis.randolph@gmail.com>
2023-03-28 17:03:24 -0500

    added 'our story' text

--- src/components/OurStory.vue
+++ src/components/OurStory.vue
@@ -1,8 +1,8 @@
 <template>
   <section id="container">
-    <div>
+    <div id="text-container">
       <h2>OUR STORY</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et <a href="#" target="_blank">dolore magna aliqua</a>.</p>
+      <p>The Greater New Orleans Foundation is the philanthropic institution dedicated to driving positive impact through philanthropy, leadership, and action in the Greater New Orleans Region. Our roots extend back to the Community Chest of New Orleans established in 1923. For over 90 years, we've connected our donors and fundholders with causes that they're passionate about to affect transformational change. It's how we help people make the most of their investment, while helping the members of our communities that need it most. <router-link class="nav-link" to="/timeline">Learn more about our story.</router-link></p>
     </div>
     <!-- <font-awesome-icon icon="fas fa-angle-down" class="icon" /> -->
   </section>
@@ -30,8 +30,12 @@ a, a:link, a:visited, a:hover, a:active {
   padding-bottom: 20px;
 } */
 #container {
-  /* replace 2nd 100px with 0 if icon goes back in */
-  padding: 100px 40px 100px 40px;
+  /* replace 100px with 0 if icon goes back in */
+  padding: 100px 0;
   background-color: #E2F6FF;
+}
+#text-container {
+  width: 80%;
+  margin: 0 auto;
 }
 </style>
