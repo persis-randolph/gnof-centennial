66c0b660b84f7aabbb0fdb3f1b106da304fc7499
persis-randolph <persis.randolph@gmail.com>
2023-04-04 14:23:46 -0500

    decrease video player height

--- src/components/Animation.vue
+++ src/components/Animation.vue
@@ -9,7 +9,7 @@
   <div class="wrapper">
     <div class="h_iframe">
       <!-- transparent image -->
-      <img class="ratio" src="http://placehold.it/16x9"/>
+      <img class="ratio" src="http://placehold.it/16x7"/>
       <!-- <video
         allow="fullscreen"
         frameBorder="0"
@@ -36,20 +36,23 @@
   width: 100%;
   height: 100%;
   margin: 0 auto;
-  background: #CCC;
+  color: white;
 }
 .h_iframe {
   position: relative;
 }
+/* set invert to 0 for black, 1 for white if using background image and not just built-in video black */
 .h_iframe .ratio {
   display: block;
   width: 100%;
   height: auto;
+  /* filter: brightness(0) invert(1); */
 }
 .h_iframe iframe {
-  position:absolute;
-  top: 0;
-  left: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   width: 100%;
   height: 100%;
 }
