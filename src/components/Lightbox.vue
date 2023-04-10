9a99ecb70fbed1588be1d62fc78d15565778fa64
persis-randolph <persis.randolph@gmail.com>
2023-04-09 23:55:19 -0500

    arraows for image toggle working correctly

--- src/components/Lightbox.vue
+++ src/components/Lightbox.vue
@@ -10,19 +10,19 @@
                     <img src="../assets/Icons_X_Light.svg" class="icon" @click="closeLightbox">
                 </div>
                 <!-- IF ISSUU EMBED -->
-                <div v-if="imageArray[imageIndex].issuuEmbed" class="embed-wrapper">
+                <div v-if="imageArray[indexToShow].issuuEmbed" class="embed-wrapper">
                     <iframe
                         class="embed-iframe"
                         allow="clipboard-write"
                         sandbox="allow-top-navigation allow-top-navigation-by-user-activation allow-downloads allow-scripts allow-same-origin allow-popups allow-modals allow-popups-to-escape-sandbox allow-forms"
                         allowfullscreen="true"
-                        :src="imageArray[imageIndex].issuuEmbed"
+                        :src="imageArray[indexToShow].issuuEmbed"
                     ></iframe>
                 </div>
                 <!-- IF VIMEO EMBED -->
-                <div v-if="imageArray[imageIndex].vimeoEmbed" class="embed-wrapper">
+                <div v-if="imageArray[indexToShow].vimeoEmbed" class="embed-wrapper">
                     <iframe
-                        :src="imageArray[imageIndex].vimeoEmbed"
+                        :src="imageArray[indexToShow].vimeoEmbed"
                         width="640"
                         height="360"
                         frameborder="0"
@@ -31,38 +31,58 @@
                     </iframe>
                 </div>
                 <!-- IF YOUTUBE EMBED -->
-                <div v-if="imageArray[imageIndex].youTubeEmbed">
+                <div v-if="imageArray[indexToShow].youTubeEmbed">
                     <iframe
                         width="560" height="315"
-                        :src="imageArray[imageIndex].youTubeEmbed + '?rel=0&modestbranding=1'"
+                        :src="imageArray[indexToShow].youTubeEmbed + '?rel=0&modestbranding=1'"
                         title="YouTube video player"
                         frameborder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
                         allowfullscreen>
                     </iframe>
                 </div>
-                <div v-if="getFileType(imageArray[imageIndex].clickThrough) === 'pdf'">
+                <div v-if="getFileType(imageArray[indexToShow].clickThrough) === 'pdf'">
                     <iframe
-                        :src="imageArray[imageIndex].clickThrough + '#view=fitH'"
+                        :src="imageArray[indexToShow].clickThrough + '#view=fitH'"
                         frameborder="1"
                         type="application/pdf"
                         width="700px"
                         height="1000px"
                     />
                 </div>
-                <img :src="imageArray[imageIndex].clickThrough" :id="imageArray[imageIndex].clickThrough" v-if="getFileType(imageArray[imageIndex].clickThrough) === 'image'">
+                <img :src="imageArray[indexToShow].clickThrough" :id="imageArray[indexToShow].clickThrough" v-if="getFileType(imageArray[indexToShow].clickThrough) === 'image'">
             </div>
-            <div class="caption" v-html="imageArray[imageIndex].description"></div>
-            <div class="image-selectors">
-                <div v-if="showLeftArrow" class="image-selector">&lt;</div>
-                <div v-if="showRightArrow" class="image-selector">&gt;</div>
+            <div
+                class="caption"
+                :class="!showLeftArrow && !showRightArrow ? 'bottom' : ''"
+                v-html="imageArray[indexToShow].description">
+            </div>
+            <div
+                v-if="showLeftArrow || showRightArrow" 
+                class="image-selectors"
+                :class="showLeftArrow || showRightArrow ? 'bottom' : ''" 
+            >
+                <div
+                    v-if="showLeftArrow"
+                    class="image-selector"
+                    @click="toggleImage(-1)"
+                >
+                    &lt;
+                </div>
+                <div
+                    v-if="showRightArrow"
+                    class="image-selector"
+                    @click="toggleImage(1)"
+                >
+                    &gt;
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-import { computed, onMounted } from 'vue'
+import { computed, ref } from 'vue'
 
 export default {
     name: 'Lightbox',
@@ -76,10 +96,6 @@ export default {
     },
     emits: ['close-lightbox'],
     setup (props, { emit }) {
-        onMounted(() => {
-            console.log('PROPS', props)
-        })
-        
         const closeLightbox = () => {
             emit('close-lightbox')
         }
@@ -97,19 +113,26 @@ export default {
             }
         }
 
+        const indexToShow = ref(props.imageIndex)
+        
         const showLeftArrow = computed(() => {
             const imageCount = props.imageArray.length
-            return imageCount > 1 && props.imageIndex !== 0
+            return imageCount > 1 && indexToShow.value !== 0
         })
-
         const showRightArrow = computed(() => {
             const imageCount = props.imageArray.length
-            return imageCount > 1 && props.imageIndex !== (imageCount - 1)
+            return imageCount > 1 && indexToShow.value !== (imageCount - 1)
         })
+        
+        const toggleImage = (change) => {
+            indexToShow.value = indexToShow.value + change
+        }
 
         return {
             getFileType,
             closeLightbox,
+            toggleImage,
+            indexToShow,
             showLeftArrow,
             showRightArrow
         }
@@ -136,43 +159,11 @@ export default {
     top: 0;
     bottom: 0;
 }
-/* .wrapper {
-  width: 100%;
-  height: 100%;
-  margin: 0 auto;
-  color: white;
-} */
-/* .h_iframe {
-  position: relative;
-} */
-/* set invert to 0 for black, 1 for white if using background image and not just built-in video black */
-/* filter: brightness(0) invert(1);
-.h_iframe .ratio {
-  display: block;
-  width: 100%;
-  height: auto;
-} */
-/* .h_iframe iframe {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 100%;
-  height: 100%;
-} */
-/* img { */
-    /* border: 0.5px solid #dedada; */
-    /* margin-bottom: 5px; */
-    /* display: block; */
-    /* max-width: 100%; */
-    /* height: auto; */
-    /* width: auto\9; */
-/* } */
 .lightbox {
     position: fixed;
     top: 50%;
     left: 50%;
-    transform: translate(-50%, -50%);
+    transform: translate(-50%, -55%);
     background-color: #04307e;
     color: #ffffff;
     z-index: 10;
@@ -198,7 +189,6 @@ export default {
     width: 100%;
     background-color: #04307e;
     transform: translateX(-5px);
-    border-radius: 0 0 8px 8px;
 }
 .image-wrapper {
     max-width: 80vw;
@@ -212,18 +202,6 @@ img {
     max-width: 80vw;
     max-height: 80vh;
 }
-/* #wrap {
-  width: 1000px;
-  height: 1000px;
-  padding: 0;
-  margin: 0;
-  overflow: hidden;
-}
-#scaled-frame {
-  width: 1000px;
-  height: 1500px;
-  border: 0px;
-} */
 .icon-container {
     display: flex;
     width: 100%;
@@ -238,9 +216,15 @@ img {
     display: flex;
     width: 100%;
     justify-content: center;
+    background-color: #04307e;
+    padding: 5px;
+    transform: translateX(-5px);
 }
 .image-selector {
     padding: 0 5px;
     cursor: pointer;
+}
+.bottom {
+    border-radius: 0 0 8px 8px;
 }
 </style>
