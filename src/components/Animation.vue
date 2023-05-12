fd28e5c06a9f8d8a1924102a91295a84ac1b4e36
persis-randolph <persis.randolph@gmail.com>
2023-05-12 13:30:31 -0500

    add autoplay for animation

--- src/components/Animation.vue
+++ src/components/Animation.vue
@@ -10,7 +10,7 @@
     <div class="h_iframe">
       <div class="inner-wrapper">
         <iframe
-          src="https://player.vimeo.com/video/815437038?h=358b0141c0&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+          src="https://player.vimeo.com/video/815437038?h=358b0141c0&autoplay=1&muted=1&badge=0&autopause=0&player_id=0&app_id=58479"
           frameborder="0"
           allow="autoplay; fullscreen; picture-in-picture"
           allowfullscreen
